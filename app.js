543129c4284c777fcbe605c034064c5a2b40548b
Chris <390074138@qq.com>
2021-03-30 21:11:33 -0700

    specify static file entry

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
-const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: 'nobodyknowsit',
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+
 app.use(express.static(path.join(__dirname, './client/build')));
 
 app.use('/', indexRouter);
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 3001, () => {
   console.log('Server Started!');
